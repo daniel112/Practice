5988557a8f914aaae81f1350d7adf6e3aeb2bbad
Daniel Yo <dnl992004@yahoo.com.au>
2018-11-01 15:56:57 -0700

    first commit

---
+++ DataStrucutureEx/Node.h
@@ -0,0 +1,49 @@
+//
+//  Node.h
+//  DataStrucutureEx
+//
+//  Created by Daniel Yo on 11/1/18.
+//  Copyright © 2018 Daniel Yo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Delegates
+
+// Utilities
+
+// Globals
+
+// Classes
+
+// Classes - Models
+
+// Classes - Views
+
+// Classes - View Controllers
+
+// Class Extensions
+
+// Definitions
+
+
+@interface Node : NSObject
+
+
+#pragma mark - Custom Delegates
+
+
+#pragma mark - Initialization
+
+
+#pragma mark - IBOutlets
+
+
+#pragma mark - Properties
+
+
+#pragma mark - Public API
+
+
+
+@end
---
+++ DataStrucutureEx/Node.m
@@ -0,0 +1,75 @@
+//
+//  Node.m
+//  DataStrucutureEx
+//
+//  Created by Daniel Yo on 11/1/18.
+//  Copyright © 2018 Daniel Yo. All rights reserved.
+//
+
+#import "Node.h"
+
+// Delegates
+
+// Utilities
+
+// Globals
+
+// Classes
+
+// Classes - Models
+
+// Classes - Views
+
+// Classes - View Controllers
+
+// Class Extensions
+
+// Definitions
+
+
+@interface Node ()
+
+
+
+@end
+
+
+
+@implementation Node
+
+
+#pragma mark - Initialization
+- (void)customInitNode {
+    
+}
+- (instancetype)init {
+    if (self = [super init]) {
+        [self customInitNode];
+        
+    }
+    return self;
+}
+
+
+
+#pragma mark - IBOutlets
+
+
+
+#pragma mark - Properties
+
+
+
+#pragma mark - Private API
+
+
+
+#pragma mark - Public API
+
+
+
+#pragma mark - Delegate Methods
+
+
+
+@end
